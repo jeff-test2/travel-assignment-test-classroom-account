68b575e7e586a7425a89221eda826b778e185eb4
test-classroom-account <137929685+test-classroom-account@users.noreply.github.com>
2023-06-27 14:49:08 -0400

    add comments

--- travel.py
+++ travel.py
@@ -4,5 +4,5 @@ name = input("Please enter a name:")
 start = input("Please enter a start:")
 end = input("Please ender a destination")
 means = input("Please enter a meanse of transportation:")
-
+# add a comment
 print(f"{name} wants to travel from {start} to {end} by {means}")
