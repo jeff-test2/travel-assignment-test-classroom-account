e9a4bcb2bf7fb73929377401cb095772748b11fb
test-classroom-account <137929685+test-classroom-account@users.noreply.github.com>
2023-06-27 14:47:19 -0400

    implement code

--- travel.py
+++ travel.py
@@ -1,9 +1,8 @@
 # add code here 
 
+name = input("Please enter a name:")
+start = input("Please enter a start:")
+end = input("Please ender a destination")
+means = input("Please enter a meanse of transportation:")
 
-# name = input("Please enter a name:")
-# start = input("Please enter a start:")
-# end = input("Please ender a destination")
-# means = input("Please enter a meanse of transportation:")
-
-# print(f"{name} wants to travel from {start} to {end} by {means}")
+print(f"{name} wants to travel from {start} to {end} by {means}")
